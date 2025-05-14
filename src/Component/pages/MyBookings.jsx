88e22e12037310112060138efa12f39f6d863928
esmotara-codec <esmotara1725@gmail.com>
2025-05-15 01:37:34 +0600

    mybnooking done

--- src/Component/pages/MyBookings.jsx
+++ src/Component/pages/MyBookings.jsx
@@ -21,7 +21,7 @@ const MyBookings = () => {
 
 	return (
 		<div className='min-h-screen bg-gray-50 py-8 px-4'>
-			<div className='max-w-md mx-auto'>
+			<div className='container mx-auto md:px-32'>
 				{appointments.length === 0 ? (
 					<div className='bg-white rounded-lg shadow p-6 text-center'>
 						<div className='text-gray-400 text-4xl mb-3'>📅</div>
@@ -34,20 +34,31 @@ const MyBookings = () => {
 						<h1 className='text-2xl font-bold text-gray-800 mb-6 text-center'>
 					My Today Appointments
 				</h1> 
+				<p>Our platform connects you with verified, experienced Lawyers across various specialties — all at your convenience.</p>
 						{appointments.map((appointment) => (
 							<div
 								key={appointment.id}
-								className='bg-white rounded-lg shadow p-4'
+								className='bg-white border border-gray-300 rounded-2xl shadow p-6'
 							>
-								<div className='flex justify-between items-center'>
+								<div className=' flex flex-col '>
+									<div className='flex flex-row justify-between mb-4  ' >
+
+									<div className='text-gray-700'>	
+										<h4 className='text-black text-[16px] font-semibold'>{appointment.name}</h4>
+										<p className='text-gray-500'>{appointment.speciality}</p>
+									</div>
+		
 									<div>
-										<p className='font-medium text-gray-800'>
-											Appointment Fee: {appointment.fee} Taka
+										<p className='font-medium text-gray-500 items-center '>
+											Appointment Fee: {appointment.consultationFee} Taka
 										</p>
 									</div>
+									</div>
+
+								
 									<button
 										onClick={() => cancelAppointment(appointment.id)}
-										className='bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded text-sm transition'
+										className=' w-full bg-white border border-[#FF0000] hover:bg-red-600 text-[#FF0000] px-3 py-1 rounded-lg  text-sm transition'
 									>
 										Cancel Appointment
 									</button>
