17bfad7047ccd95fc162b5618d00dc6e8b9938c9
esmotara-codec <esmotara1725@gmail.com>
2025-06-24 12:00:00 +0600

    Commit on June 24, 2025

--- src/Component/NavBar/DynamicLink.jsx
+++ src/Component/NavBar/DynamicLink.jsx
@@ -6,7 +6,7 @@ const DynamicLink = ({ route }) => {
             <li className='px-3 p-2 text-black'>
                 <NavLink to={route.path}
                     className={({ isActive }) =>
-                        `block px-2 py-1 rounded hover:bg-gray-200 ${isActive ? " font-semibold" : ""
+                        `block px-2 py-1 rounded hover:bg-gray-200 ${isActive ? "font-semibold" : ""
                         }`
                     }
                 >{route.name}</NavLink>
